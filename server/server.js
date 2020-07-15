7dc9cb198499bc74148254fb2caad95b95014842
mrdedede <acasf@cin.ufpe.br>
2020-07-14 23:36:55 -0300

    Finnishing the REST API
    Also could connect the front end to the back end.

---
+++ server/server.js
@@ -0,0 +1,192 @@
+const express = require('express')
+const fs = require('fs')
+const cors = require('cors')
+
+const app = express()
+app.options('*', cors())
+
+/**
+ * Loads the game .json standard
+ */
+function loadGameStyle() {
+  let gameExample = fs.readFileSync('game_example.json','utf8')
+  return JSON.parse(gameExample)
+}
+
+/**
+ * Generate a room code based of a random integer 
+ */
+function generateRoomCode() {
+  return Math.floor(Math.random()*100)
+}
+
+/**
+ * Creates a new game object and writes it on the database
+ * @param {string} pl1 - The player1's name
+ * @param {string} pl2 - The player2's name
+ */
+function newGame(pl1, pl2, roomCode) {
+  let gamesSoFar = JSON.parse(fs.readFileSync('games.json', 'utf8'))
+  let newGame = loadGameStyle()
+  newGame.roomcode = roomCode
+  newGame.player1.playerName = pl1
+  newGame.player1.isTurn = true
+  newGame.player2.playerName = pl2
+  gamesSoFar.push(newGame)
+  fs.writeFileSync('games.json', JSON.stringify(gamesSoFar))
+  return newGame
+}
+
+/**
+ * Deletes a game
+ * @param {number} code - The game's code
+ */
+function deleteGame(code) {
+  let gamesSoFar = JSON.parse(fs.readFileSync('games.json', 'utf8'))
+  let newGameArray = []
+  gamesSoFar.forEach(game => {
+    if(game.roomcode != code) {
+      newGameArray.push(game)
+    }
+  })
+  fs.writeFileSync('games.json', JSON.stringify(newGameArray))
+}
+
+/**
+ * Makes a round go around in the game
+ * @param {number} x - Selected Column
+ * @param {number} y - selected Row
+ * @param {string} player - Is it an X or an O?
+ * @param {number} code - Room's code
+ */
+function play(x, y, player, code) {
+  let gamesSoFar = JSON.parse(fs.readFileSync('games.json', 'utf8'))
+  let lastGame
+  gamesSoFar.forEach(game => {
+    if(game.roomcode == code) {
+      game.board[x][y] = player
+      if(game.player1.isTurn) {
+        game.player1.isTurn = false
+        game.player2.isTurn = true
+      } else {
+        game.player1.isTurn = true
+        game.player2.isTurn = false
+      }
+      game = hasEnded(game)
+      lastGame = game
+    }
+  })
+  fs.writeFileSync('games.json', JSON.stringify(gamesSoFar))
+  return lastGame
+}
+
+/**
+ * Checks if a game has ended
+ * @param {object} game - Game that we're trying to judge if there's a winner
+ */
+function hasEnded(game) {
+  //Function not perfectly implemented, since it's basically legacy code
+  let hasWinner = false;
+  let winner = null;
+  for(let i = 0; i < 3; i++){
+    if((game.board[i][0] == game.board[i][1])&&(game.board[i][0] == game.board[i][2])
+      &&(game.board[i][0] != null)) {
+      hasWinner = true
+      winner = game.board[i][0]
+    } else if((game.board[0][i] == game.board[1][i])&&
+      (game.board[0][i] == game.board[2][i])&&(game.board[0][i] != null)) {
+      hasWinner = true
+      winner = game.board[0][i]
+    }
+  }
+  if(!(winner)) {
+    if((game.board[0][0] == game.board[1][1])&&
+      (game.board[0][0] == game.board[2][2])&&(game.board[0][0] != null)) {
+      hasWinner = true
+      winner = game.board[0][0]
+    } else if((game.board[0][2] == game.board[1][1])&&
+      (game.board[2][0] == game.board[0][2])&&(game.board[0][2] != null)) {
+      hasWinner = true
+      winner = game.board[0][2]
+    }
+  }
+  game.ended = hasWinner
+  game.winner = winner
+  return game
+}
+
+/**
+ * Puts a player in the waitlist
+ * @param {string} playerName - The player's name
+ */
+function wait(playerName) {
+  let players = JSON.parse(fs.readFileSync('players.json'))
+  if(players.length == 0) {
+    players.push({playerName: playerName, isPlaying: false, gameCode: null})
+  } else {
+    let roomCode = generateRoomCode()
+    players[0].isPlaying = true
+    players[0].gameCode = roomCode
+    let player = {playerName: playerName, isPlaying: true, gameCode: roomCode}
+    players.push(player)
+    fs.writeFileSync(JSON.stringify(players))
+  }
+  return true
+}
+
+/**
+ * Takes a player out of the waitlist
+ * @param {string} playerName - The player's name 
+ */
+function checkWaitlist(playerName) {
+  let players = JSON.parse(fs.readFileSync('players.json'))
+  let gameCode = null
+  let newWaitlist = []
+  players.forEach(player => {
+    if(player.playerName != playerName) {
+      newWaitlist.push(player)
+    } else {
+      gameCode = player.gameCode
+      if(gameCode == null) {
+        newWaitlist.push({playerName: playerName, isPlaying: true, gameCode: gameCode})
+      } else {
+        let games = JSON.parse(fs.readFileSync('games.json'))
+        playing_now = []
+        players.forEach(curPlayer => {
+          if(curPlayer.gameCode == gameCode) {
+            playing_now.push(curPlayer.playerName)
+          }
+        })
+        newGame(playing_now[0], playing_now[1], gameCode)
+      }
+    }
+  })
+  fs.writeFileSync(JSON.stringify(newWaitlist))
+  return gameCode
+}
+
+app.post('/waitlist/join/:player', function(req, res) {
+  return wait(req.params.player)
+})
+
+app.post('/waitlist/check/:player', function(req, res) {
+  return checkWaitlist(req.params.player)
+})
+
+app.post('/game/play/:code/:player/:x/:y', function(req, res) {
+  return play(req.params.x, req.params.y, req.params.player, req.params.code)
+})
+
+app.delete('/game/delete/:code', function(req, res) {
+  deleteGame(req.params.code)
+})
+
+app.get('/', function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*')
+  console.log("hmmm")
+  return res.send('Hello world')
+})
+
+app.listen(2000, function() {
+  console.log("running")
+})
--- tictactoe.js
+++ tictactoe.js
@@ -1,160 +1,170 @@
-var scr = document.getElementById("ttc");
-var ctx = scr.getContext("2d");
-ctx.strokeRect(0, 0, 100, 100);
-ctx.strokeRect(100, 0, 100, 100);
-ctx.strokeRect(200, 0, 100, 100);
-ctx.strokeRect(0, 100, 100, 100);
-ctx.strokeRect(100, 100, 100, 100);
-ctx.strokeRect(200, 100, 100, 100);
-ctx.strokeRect(0, 200, 100, 100);
-ctx.strokeRect(100, 200, 100, 100);
-ctx.strokeRect(200, 200, 100, 100);
+const http = new XMLHttpRequest()
 
-class Board{
-    constructor(){
-        this.gboard = [[null, null, null],
-                [null, null, null],
-                [null, null, null]];
-    }
-    check(){
-        var winner = false;
-        var champion = null;
-        for(var i = 0; i < 3; i++){
-            if((this.gboard[i][0] == this.gboard[i][1]) && (this.gboard[i][0] == this.gboard[i][2]) &&(this.gboard[i][0] != null)){
-                winner = true;
-                champion = this.gboard[i][0];
-            }
-            else if((this.gboard[0][i] == this.gboard[1][i])&&(this.gboard[0][i] == this.gboard[2][i])&&(this.gboard[0][i] != null)){
-                winner = true;
-                champion = this.gboard[0][i];
-            }
-        }
-        if(!(winner)){
-            if((this.gboard[0][0] == this.gboard[1][1])&&(this.gboard[0][0] == this.gboard[2][2])&&(this.gboard[0][0] != null)){
-                winner = true;
-                champion = this.gboard[0][0];
-            }
-            else if((this.gboard[0][2] == this.gboard[1][1])&&(this.gboard[2][0] == this.gboard[0][2])&&(this.gboard[0][2] != null)){
-                winner = true;
-                champion = this.gboard[0][2];
-            }
-        }
-        return [winner, champion];
-    }
+http.open("GET", "http://localhost:2000")
+http.send()
 
-    post_check(){
-        var results = this.check()
-        if(results[0] == false){
-            var procceed = false;
-            for(var i = 0; i < 3; i++){
-                for(var j = 0; j < 3; j++){
-                    if(this.gboard[i][j] == null){
-                        procceed = true;
-                    }
-                }
-            }
-            if(!procceed){
-                return false;
-            }
-            else{
-                return true;
-            }
-        }
-        else{
-            return results[1]
-        }
-    }
+http.onload = () => console.log(http.responseText)
 
-    play(x,y,subs){
-        var played = false
-        if(this.gboard[x][y] == null){
-            this.gboard[x][y] = subs;
-            played = true;
-        }
-        return played;
-    }
-}
-var board = new Board();
-scr.addEventListener('mousedown',onDown,false);
-var menu = document.getElementById("menu");
-var who = "";
-function transform(c){
-    if(c < 100){
-        var p = 0;
-    }
-    else if(c < 200){
-        var p = 1;
-    }
-    else{
-        var p = 2;
-    }
-    return p;
-}
-var who = "O";
+// var scr = document.getElementById("ttc");
+// var ctx = scr.getContext("2d");
+// ctx.strokeRect(0, 0, 100, 100);
+// ctx.strokeRect(100, 0, 100, 100);
+// ctx.strokeRect(200, 0, 100, 100);
+// ctx.strokeRect(0, 100, 100, 100);
+// ctx.strokeRect(100, 100, 100, 100);
+// ctx.strokeRect(200, 100, 100, 100);
+// ctx.strokeRect(0, 200, 100, 100);
+// ctx.strokeRect(100, 200, 100, 100);
+// ctx.strokeRect(200, 200, 100, 100);
 
-function whos_next(alpha){
-    if(alpha == "X"){
-        alpha = "O";
-    }
-    else{
-        alpha = "X";
-    }
-    return alpha;
-}
+// //This class shall be mostly reformulated, since it is implemented in the API
+// class Board{
+//     constructor(){
+//         this.gboard = [[null, null, null],
+//                 [null, null, null],
+//                 [null, null, null]];
+//     }
+//     //This function shall be removed, since it is implemented in the API
+//     check(){
+//         var winner = false;
+//         var champion = null;
+//         for(var i = 0; i < 3; i++){
+//             if((this.gboard[i][0] == this.gboard[i][1])&&(this.gboard[i][0] == this.gboard[i][2])
+//               &&(this.gboard[i][0] != null)){
+//                 winner = true;
+//                 champion = this.gboard[i][0];
+//             }
+//             else if((this.gboard[0][i] == this.gboard[1][i])&&
+//               (this.gboard[0][i] == this.gboard[2][i])&&(this.gboard[0][i] != null)){
+//                 winner = true;
+//                 champion = this.gboard[0][i];
+//             }
+//         }
+//         if(!(winner)){
+//             if((this.gboard[0][0] == this.gboard[1][1])&&
+//               (this.gboard[0][0] == this.gboard[2][2])&&(this.gboard[0][0] != null)){
+//                 winner = true;
+//                 champion = this.gboard[0][0];
+//             }
+//             else if((this.gboard[0][2] == this.gboard[1][1])&&
+//               (this.gboard[2][0] == this.gboard[0][2])&&(this.gboard[0][2] != null)){
+//                 winner = true;
+//                 champion = this.gboard[0][2];
+//             }
+//         }
+//         return [winner, champion];
+//     }
 
-function onDown(event){
-    cx = event.pageX - (scr.offsetLeft + 8); //8 is there due to the minimum distance of the
-    cy = event.pageY - (scr.offsetTop + menu.offsetHeight + 18); //18 is there due to the header of the page, feel free to change it
-    px = transform(cx);
-    py = transform(cy);
-    var played = window.board.play(px,py,who);
-    if(played){
-        draw(who, px, py);
-        who = whos_next(who);
-        var results = window.board.post_check();
-        if(!(results === true)){
-            document.getElementById("restart").style.visibility = "visible";
-            if(results == "X"){
-                alert("The square wins! Restart the game clicking the button bellow!");
-            }
-            else if(results == "O"){
-                alert("The ball wins! Restart the game clicking the button bellow!");
-            }
-            else{
-                alert("It's a tie! Restart the game clicking the button bellow!");
-            }
-        }
-    }
-    else{
-        alert("You can't play here!");
-    }
-}
-function drawO(px,py){
-    ctx.beginPath();
-    ctx.arc(px*100 + 50, py*100 + 50, 30, 0, 2 * Math.PI, false);
-    ctx.stroke();
-}
-function drawX(px,py){
-    ctx.strokeRect(px*100 + 25, py*100 + 25, 50, 50);
-}
-function draw(who, px, py){
-    if(who == "O"){
-        drawO(px,py);
-    }
-    else{
-        drawX(px,py);
-    }
-}
-function restart(){
-    board = new Board();
-    ctx.clearRect (0, 0, 300, 300);
-    ctx.strokeRect(0, 0, 100, 100);
-    ctx.strokeRect(100, 0, 100, 100);
-    ctx.strokeRect(200, 0, 100, 100);
-    ctx.strokeRect(0, 100, 100, 100);
-    ctx.strokeRect(100, 100, 100, 100);
-    ctx.strokeRect(200, 100, 100, 100);
-    ctx.strokeRect(0, 200, 100, 100);
-    ctx.strokeRect(100, 200, 100, 100);
-    ctx.strokeRect(200, 200, 100, 100);
-}
+//     post_check(){
+//         var results = this.check()
+//         if(results[0] == false){
+//             var procceed = false;
+//             for(var i = 0; i < 3; i++){
+//                 for(var j = 0; j < 3; j++){
+//                     if(this.gboard[i][j] == null){
+//                         procceed = true;
+//                     }
+//                 }
+//             }
+//             if(!procceed) {
+//                 return false;
+//             } else {
+//                 return true;
+//             }
+//         } else {
+//             return results[1]
+//         }
+//     }
+
+//     play(x,y,subs){
+//         var played = false
+//         if(this.gboard[x][y] == null){
+//             this.gboard[x][y] = subs;
+//             played = true;
+//         }
+//         return played;
+//     }
+// }
+// var board = new Board();
+// scr.addEventListener('mousedown',onDown,false);
+// var who = "";
+// function transform(c){
+//     if(c < 100){
+//         var p = 0;
+//     }
+//     else if(c < 200){
+//         var p = 1;
+//     }
+//     else{
+//         var p = 2;
+//     }
+//     return p;
+// }
+// var who = "O";
+
+// function whos_next(alpha){
+//     if(alpha == "X"){
+//         alpha = "O";
+//     }
+//     else{
+//         alpha = "X";
+//     }
+//     return alpha;
+// }
+
+// function onDown(event){
+//     cx = event.pageX - (scr.offsetLeft + 8); //8 is there due to the minimum distance of the
+//     cy = event.pageY - (scr.offsetTop + 18); //18 is there due to the header of the page, feel free to change it
+//     px = transform(cx);
+//     py = transform(cy);
+//     var played = window.board.play(px,py,who);
+//     if(played){
+//         draw(who, px, py);
+//         who = whos_next(who);
+//         var results = window.board.post_check();
+//         if(!(results === true)){
+//             document.getElementById("restart").style.visibility = "visible";
+//             if(results == "X"){
+//                 alert("The square wins! Restart the game clicking the button bellow!");
+//             }
+//             else if(results == "O"){
+//                 alert("The ball wins! Restart the game clicking the button bellow!");
+//             }
+//             else{
+//                 alert("It's a tie! Restart the game clicking the button bellow!");
+//             }
+//         }
+//     }
+//     else{
+//         alert("You can't play here!");
+//     }
+// }
+// function drawO(px,py){
+//     ctx.beginPath();
+//     ctx.arc(px*100 + 50, py*100 + 50, 30, 0, 2 * Math.PI, false);
+//     ctx.stroke();
+// }
+// function drawX(px,py){
+//     ctx.strokeRect(px*100 + 25, py*100 + 25, 50, 50);
+// }
+// function draw(who, px, py){
+//     if(who == "O"){
+//         drawO(px,py);
+//     }
+//     else{
+//         drawX(px,py);
+//     }
+// }
+// function restart(){
+//     board = new Board();
+//     ctx.clearRect (0, 0, 300, 300);
+//     ctx.strokeRect(0, 0, 100, 100);
+//     ctx.strokeRect(100, 0, 100, 100);
+//     ctx.strokeRect(200, 0, 100, 100);
+//     ctx.strokeRect(0, 100, 100, 100);
+//     ctx.strokeRect(100, 100, 100, 100);
+//     ctx.strokeRect(200, 100, 100, 100);
+//     ctx.strokeRect(0, 200, 100, 100);
+//     ctx.strokeRect(100, 200, 100, 100);
+//     ctx.strokeRect(200, 200, 100, 100);
+// }
